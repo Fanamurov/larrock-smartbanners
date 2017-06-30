956d2b2444a2577c461d80e4c2832034d418a397
Alexandr Fanamurov <fanamura@gmail.com>
2017-06-30 17:49:21 +1000

    Fix

--- src/LarrockComponentSmartbannersServiceProvider.php
+++ src/LarrockComponentSmartbannersServiceProvider.php
@@ -17,7 +17,7 @@ class LarrockComponentSmartbannersServiceProvider extends ServiceProvider
         $this->loadViewsFrom(__DIR__.'/views', 'larrock');
 
         $this->publishes([
-            __DIR__.'/views' => base_path('resources/views/larrock'),
+            __DIR__.'/views' => base_path('resources/views/vendor/larrock')
         ]);
     }
 
